4d65ca127980576abb4e1d7a3f85ba79e5d1e57d
Keith Noguchi <keith@noguchi.us>
2023-03-06 21:25:15 -0800

    Listen on the port 3000

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+use std::net::TcpListener;
+
+fn main() -> eyre::Result<()> {
+    let listener = TcpListener::bind("127.0.0.1:3000")?;
+
+    for stream in listener.incoming() {
+        let stream = stream?;
+        println!("{stream:#?}");
+    }
+    Ok(())
 }
