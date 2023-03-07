de738827aeb5266f964a8a87e3f2b098204f41a4
Keith Noguchi <keith@noguchi.us>
2023-03-06 22:21:26 -0800

    Listen on the port 3000 (#2)
    * Listen on the port 3000
    * Parse the request line and the headers
    * Make it work with MSRV 1.57

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
-fn main() {
-    println!("Hello, world!");
+use std::error::Error;
+use std::io::{BufRead, BufReader};
+use std::net::{TcpListener, TcpStream};
+use std::result;
+
+type Result<T> = result::Result<T, Box<dyn Error + Send + Sync + 'static>>;
+
+fn main() -> Result<()> {
+    let listener = TcpListener::bind("127.0.0.1:3000")?;
+
+    for stream in listener.incoming() {
+        handle_connection(stream?)?;
+    }
+    Ok(())
+}
+
+fn handle_connection(mut stream: TcpStream) -> Result<()> {
+    let mut reader = BufReader::new(&mut stream).lines();
+
+    // request line.
+    let req_line = reader.next().ok_or("missing request line")??;
+    println!("{:#?}", req_line);
+
+    // headers.
+    let mut headers = vec![];
+    for line in reader {
+        let line = line?;
+        if line.is_empty() {
+            break;
+        }
+        headers.push(line);
+    }
+    println!("{:#?}", headers);
+
+    Ok(())
 }
