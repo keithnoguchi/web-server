c595f809f5fd88576aec45188efd0ea7d4b01679
Keith Noguchi <keith@noguchi.us>
2023-03-08 12:18:23 -0800

    Reads the page from index.html (#8)
    Unconditional static page response.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::error::Error;
+use std::fs;
 use std::io::{BufRead, BufReader, Write};
 use std::net::{TcpListener, TcpStream};
 use std::result;
@@ -17,15 +18,22 @@ fn main() -> Result<()> {
 }
 
 fn handle_connection(mut s: TcpStream) -> Result<()> {
+    // request.
     let req: Vec<_> = BufReader::new(&mut s)
         .lines()
         .map(|line| line.unwrap())
         .take_while(|line| !line.is_empty())
         .collect();
-
     dbg!(req);
 
-    let resp = "HTTP/1.1 200 OK\r\n\r\n";
+    // response.
+    let status_line = "HTTP/1.1 200 OK";
+    let body = fs::read_to_string("index.html")?;
+    let body_len = body.len();
+    let resp = format!("{status_line}\r\nContent-Length: {body_len}\r\n\r\n{body}");
+
+    dbg!(&resp);
+
     s.write_all(resp.as_bytes())?;
 
     Ok(())
