111e04fc4246df97ddd48f18f6b2ee97b26b3d1c
Keith Noguchi <keith@noguchi.us>
2023-03-08 15:47:19 -0800

    Add tracing

--- src/main.rs
+++ src/main.rs
@@ -7,11 +7,15 @@ use std::sync::mpsc::{sync_channel, SyncSender};
 use std::sync::{Arc, Mutex};
 use std::thread::{self, JoinHandle};
 
+use tracing::{debug, error, instrument};
+
 type Result<T> = result::Result<T, Box<dyn Error + Send + Sync + 'static>>;
 
 fn main() -> Result<()> {
+    tracing_subscriber::fmt::init();
+
     let listener = TcpListener::bind("127.0.0.1:3000")?;
-    let pool = ThreadPool::new(4);
+    let pool = ThreadPool::new(5);
 
     // only take 5 sessions.
     for s in listener.incoming().take(5) {
@@ -22,12 +26,35 @@ fn main() -> Result<()> {
     Ok(())
 }
 
+#[instrument(level = "info", ret, err)]
+fn handle_connection(mut s: TcpStream) -> Result<()> {
+    debug!(?s, "handling connection");
+    // A request.
+    let req_line = BufReader::new(&mut s)
+        .lines()
+        .next()
+        .ok_or("invalid HTTP request line")??;
+
+    // A response.
+    let (status, file) = match req_line.as_str() {
+        "GET / HTTP/1.1" => ("HTTP/1.1 200 OK", "index.html"),
+        _ => ("HTTP/1.1 404 Not Found", "404.html"),
+    };
+    let content = fs::read_to_string(file)?;
+    let len = content.len();
+    let resp = format!("{status}\r\nContent-Length: {len}\r\n\r\n{content}");
+    s.write_all(resp.as_bytes())?;
+
+    Ok(())
+}
+
 struct ThreadPool {
     workers: Vec<JoinHandle<()>>,
     tx: Option<SyncSender<Box<dyn FnOnce() -> Result<()> + Send + 'static>>>,
 }
 
 impl Drop for ThreadPool {
+    #[instrument(skip(self))]
     fn drop(&mut self) {
         if let Some(tx) = self.tx.take() {
             drop(tx);
@@ -41,23 +68,26 @@ impl Drop for ThreadPool {
 }
 
 impl ThreadPool {
+    #[instrument]
     fn new(size: usize) -> Self {
         let (tx, rx) = sync_channel::<Box<dyn FnOnce() -> Result<()> + Send + 'static>>(size);
         let tx = Some(tx);
         let rx = Arc::new(Mutex::new(rx));
         let workers: Vec<_> = (0..size)
-            .map(|_| {
+            .map(|id| {
                 let rx = rx.clone();
                 thread::spawn(move || loop {
                     let f = rx.lock().unwrap().recv();
                     match f {
                         Ok(f) => {
+                            debug!(%id, "start handler");
                             if let Err(e) = f() {
-                                dbg!(e);
+                                error!(%id, error = ?e, "handle error");
                             }
+                            debug!(%id, "finish handler");
                         }
                         Err(e) => {
-                            dbg!(e);
+                            error!(%id, error = ?e, "channel error");
                             return;
                         }
                     }
@@ -67,6 +97,7 @@ impl ThreadPool {
         Self { workers, tx }
     }
 
+    #[instrument(skip(self, f))]
     fn execute<F>(&self, f: F)
     where
         F: FnOnce() -> Result<()> + Send + 'static,
@@ -77,26 +108,4 @@ impl ThreadPool {
             }
         }
     }
-}
-
-fn handle_connection(mut s: TcpStream) -> Result<()> {
-    // A request.
-    let req_line = BufReader::new(&mut s)
-        .lines()
-        .next()
-        .ok_or("invalid HTTP request line")??;
-
-    dbg!(&req_line);
-
-    // A response.
-    let (status, file) = match req_line.as_str() {
-        "GET / HTTP/1.1" => ("HTTP/1.1 200 OK", "index.html"),
-        _ => ("HTTP/1.1 404 Not Found", "404.html"),
-    };
-    let content = fs::read_to_string(file)?;
-    let len = content.len();
-    let resp = format!("{status}\r\nContent-Length: {len}\r\n\r\n{content}");
-    s.write_all(resp.as_bytes())?;
-
-    Ok(())
 }
