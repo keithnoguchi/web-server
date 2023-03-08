fb59634fccb93f17ec6fcac5965f769f5486eb5e
Keith Noguchi <keith@noguchi.us>
2023-03-07 17:24:16 -0800

    Connection establishment (#6)
    It accepts a connection and prints it out on the stdout.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
-fn main() {
-    println!("Hello, world!");
+use std::error::Error;
+use std::io::{BufRead, BufReader};
+use std::net::{TcpListener, TcpStream};
+use std::result;
+
+type Result<T> = result::Result<T, Box<dyn Error + Send + Sync + 'static>>;
+
+fn main() -> Result<()> {
+    let listener = TcpListener::bind("127.0.0.1:3000")?;
+
+    for stream in listener.incoming() {
+        let mut stream = stream?;
+        handle_connection(&mut stream)?;
+    }
+
+    Ok(())
+}
+
+fn handle_connection(s: &mut TcpStream) -> Result<()> {
+    let req: Vec<_> = BufReader::new(s)
+        .lines()
+        .map(|line| line.unwrap())
+        .take_while(|line| !line.is_empty())
+        .collect();
+
+    println!("{:#?}", req);
+
+    Ok(())
 }
