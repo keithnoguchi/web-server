d04550ffdbcdff26949ee77ec0d9ee241cdf2b8c
Keith Noguchi <keith@noguchi.us>
2023-03-12 18:25:41 -0700

    A single threaded server

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 //! A web server
 
 use std::error::Error;
-use std::io::{BufRead, BufReader};
+use std::fs;
+use std::io::{BufRead, BufReader, Write};
 use std::net::{TcpListener, TcpStream};
 use std::result;
 
@@ -12,7 +13,9 @@ fn main() -> Result<()> {
 
     for stream in listener.incoming() {
         let stream = stream?;
-        handler(stream);
+        if let Err(e) = handler(stream) {
+            eprintln!("{e}");
+        }
     }
 
     Ok(())
@@ -20,12 +23,17 @@ fn main() -> Result<()> {
 
 fn handler(mut s: TcpStream) -> Result<()> {
     let reader = BufReader::new(&mut s);
-    let req_line = reader
-        .lines()
-        .next()
-        .ok_or("invalid request line")??;
+    let req_line = reader.lines().next().ok_or("invalid request line")??;
 
-    println!("{req_line}");
+    let (status, file) = match req_line.as_ref() {
+        "GET / HTTP/1.1" => ("HTTP/1.1 200 OK", "index.html"),
+        _ => ("HTTP/1.1 404 NOT FOUND", "404.html"),
+    };
+    let content = fs::read_to_string(file)?;
+    let length = content.len();
+    let resp = format!("{status}\r\nContent-Length: {length}\r\n\r\n{content}");
+
+    s.write_all(resp.as_bytes())?;
 
     Ok(())
 }
